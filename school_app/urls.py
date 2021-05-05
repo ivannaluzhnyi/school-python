bf35ff09c412efe36a3ed0e59b2153e9f6272c5f
Ivan Naluzhnyi <ivan.naluzhnyi@gmail.com>
2021-05-05 15:28:48 +0200

    add pages

--- school_app/urls.py
+++ school_app/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 
-from . import views
+from .views import HomePageView
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('home/', HomePageView.as_view(), name='home'),
 ]
