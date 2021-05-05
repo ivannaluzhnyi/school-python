450fd3bced31229b344e8523db706de45e131f6a
Ivan Naluzhnyi <ivan.naluzhnyi@gmail.com>
2021-05-05 14:45:36 +0200

    feat(template-static): add html files

--- python_projet/settings.py
+++ python_projet/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'school_app.apps.SchoolAppConfig'
+    'school_app.apps.SchoolAppConfig',
+    'bootstrap4'
 ]
 
 MIDDLEWARE = [
