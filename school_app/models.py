dd60ac36f5341b3a459b4b71e8c235090fe55fc0
Ivan Naluzhnyi <ivan.naluzhnyi@gmail.com>
2021-05-05 11:58:52 +0200

    feat(models): up modesl

--- school_app/models.py
+++ school_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Subject(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.TextField()
+    classes = models.ManyToManyField(Class)
+
+class Class(models.Model):
+    name = models.CharField(max_length=200)
+    promotion = models.ForeignKey(Promotion, on_delete=models.CASCADE)
+    subjects = models.ManyToManyField(Subject)
+
+class Promotion(models.Model):
+    name = models.CharField(max_length=200)
+    classes = models.OneToManyField(Class)
+
+class Note(models.Model):
+    note = models.IntegerField(max_length=11)
+    subject = models.ForeignKey(Subject, on_delete=models.CASCADE)
+    
+
