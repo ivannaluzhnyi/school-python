d2dd6fef62e82b2eee1081a2de29c9d6e52c7a0f
Rémi FAVIEZ <faviezremi98@gmail.com>
2021-05-06 16:10:55 +0200

    untracked files

---
+++ school_app/forms/user_update_form.py
@@ -0,0 +1,18 @@
+from django import forms
+from django.contrib.auth.models import User
+
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta():
+        model = User
+        exclude = ['password', 'username', 'is_staff', 'user_permissions', 'is_active', 'is_superuser', 'date_joined']  # ['created_by', ]
+        fields = '__all__'  # ['name', 'language']
+
+
+    def clean_username(self):
+        
+        return data
+
+    def clean_password(self):
+        
+        return data
